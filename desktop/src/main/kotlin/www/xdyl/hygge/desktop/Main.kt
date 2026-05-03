4de1fb69530af93279d331ea24aefb34603aef2a
UNSA-studio <UNSA-studio@outlook.com>
2026-05-03 22:23:22 +0800

    Fix font loading API for Windows

--- desktop/src/main/kotlin/www/xdyl/hygge/desktop/Main.kt
+++ desktop/src/main/kotlin/www/xdyl/hygge/desktop/Main.kt
@@ -1,7 +1,6 @@
 package www.xdyl.hygge.desktop
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -12,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
