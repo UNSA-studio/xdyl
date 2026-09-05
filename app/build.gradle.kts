1880f4ab69e637438f170534affefe176a8af312
UNSA-studio <UNSA-studio@outlook.com>
2026-09-05 23:37:26 +0000

    fix(app): add desugar_jdk_libs dependency for coreLibraryDesugaring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
